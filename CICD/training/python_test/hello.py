6c199efaf91cd14e2d4747cf5e1e65ff72b3a88e
jjang-go <tykim9532@gmail.com>
2024-03-13 15:20:40 +0900

    Update pylint, test, coverage

--- CICD/training/python_test/hello.py
+++ CICD/training/python_test/hello.py
@@ -1,7 +1,14 @@
+"""
+패스트 캠퍼스 API
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """
+    hello world api
+    """
     return "<p>Hello, World!</p>"
